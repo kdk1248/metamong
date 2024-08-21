f1acdee8dc7505b9f2d27ee8bb388f864848fcd4
kdk1248 <alice3976@g.hongik.ac.kr>
2024-08-21 18:40:55 +0900

    환경변수 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,31 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReviewModule } from './res/review/review.module';
 import { ReviewEntity } from './res/review/entity/review.entity';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 //UserModule을 생성했기 때문에 AppModule에 등록을 해야 동작하게끔 만듦
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        retryAttempts: configService.get('NODE_ENV') === 'prod' ? 10 : 1,
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: Number(configService.get('DB_PORT')),
+        database: configService.get('DB_NAME'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        entities: [
+          path.join(__dirname, 'src/entities/**/*.entity.{js, ts}'),
+        ],
+        synchronize: false,
+        logging: true,
+        timezone: 'local',
+      }),
     }),
     TypeOrmModule.forRoot({
       type: 'mysql', // 사용하려는 데이터베이스 타입
