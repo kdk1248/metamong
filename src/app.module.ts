21243c153699f310a739b346585d4c0724685e03
kdk1248 <alice3976@g.hongik.ac.kr>
2024-08-21 17:57:43 +0900

    config 관련 코드 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as path from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReviewModule } from './res/review/review.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Review } from './res/review/entity/review.entity';
+import { ReviewEntity } from './res/review/entity/review.entity';
+import { ConfigModule } from '@nestjs/config';
+
 //UserModule을 생성했기 때문에 AppModule에 등록을 해야 동작하게끔 만듦
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql', // 사용하려는 데이터베이스 타입
       host: 'localhost',
@@ -14,10 +20,10 @@ import { Review } from './res/review/entity/review.entity';
       username: 'root',
       password: '1248',
       database: 'movie',
-      entities: [Review], // 엔티티 목록
-      synchronize: true, // 개발 환경에서만 사용, 자동으로 데이터베이스 스키마를 동기화합니다.
+      entities: [ReviewEntity], // 엔티티 목록
+      synchronize: false, // 개발 환경에서만 사용, 자동으로 데이터베이스 스키마를 동기화합니다.
     }),
-    TypeOrmModule.forFeature([Review]), // 특정 엔티티에 대한 레포지토리 등록
+    TypeOrmModule.forFeature([ReviewEntity]), // 특정 엔티티에 대한 레포지토리 등록
     ReviewModule
   ],
   controllers: [AppController],
