167be7d0c234c4c0695df88c9b48deff9c4df2ee
seojin <1020seojin@naver.com>
2024-08-21 18:43:31 +0900

    중복 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -31,16 +31,6 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         timezone: 'local',
       }),
     }),
-    TypeOrmModule.forRoot({
-      type: 'mysql', // 사용하려는 데이터베이스 타입
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '1248',
-      database: 'movie',
-      entities: [ReviewEntity], // 엔티티 목록
-      synchronize: false, // 개발 환경에서만 사용, 자동으로 데이터베이스 스키마를 동기화합니다.
-    }),
     TypeOrmModule.forFeature([ReviewEntity]), // 특정 엔티티에 대한 레포지토리 등록
     ReviewModule
   ],
