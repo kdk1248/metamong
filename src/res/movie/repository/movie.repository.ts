3fdb0a79b3b7e165cdca698c623c15c45cad1093
kdk1248 <alice3976@g.hongik.ac.kr>
2024-08-22 18:57:42 +0900

    movie 생성자 코드 추가

--- src/res/movie/repository/movie.repository.ts
+++ src/res/movie/repository/movie.repository.ts
@@ -1,11 +1,17 @@
-import { Repository, LessThan, Between, FindOptionsOrder } from 'typeorm';
+import { Repository, LessThan, Between } from 'typeorm';
 import { MovieEntity } from '../entity/movie.entity';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class MovieRepository extends Repository<MovieEntity> {
+export class MovieRepository {
+    constructor(
+        @InjectRepository(MovieEntity)
+        private readonly movieRepository: Repository<MovieEntity>,
+    ){}
+
     async findAllOrderByModifiedAtDesc(): Promise<MovieEntity[]> {
-        return this.find({
+        return this.movieRepository.find({
             order: {
                 modifiedAt: 'DESC',
             },
@@ -13,7 +19,7 @@ export class MovieRepository extends Repository<MovieEntity> {
     }
 
     async findMoviesReleasedBefore(date: Date): Promise<MovieEntity[]> {
-        return this.find({
+        return this.movieRepository.find({
             where: {
                 releasedAt: LessThan(date),
             },
@@ -21,7 +27,7 @@ export class MovieRepository extends Repository<MovieEntity> {
     }
 
     async findMoviesReleasedBetween(startDate: Date, endDate: Date): Promise<MovieEntity[]> {
-        return this.find({
+        return this.movieRepository.find({
             where: {
                 releasedAt: Between(startDate, endDate),
             },
