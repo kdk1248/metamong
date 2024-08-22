7f09f86ed3f2ce665cae6efe438cd708b9e151f2
kdk1248 <alice3976@g.hongik.ac.kr>
2024-08-22 18:56:19 +0900

    MovieEntity 추가

--- src/res/movie/movie.module.ts
+++ src/res/movie/movie.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { MovieController } from './controller/movie.controller';
 import { MovieService } from './service/movie.service';
 import { MovieRepository } from './repository/movie.repository';
+import { MovieEntity } from './entity/movie.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([MovieRepository])],
+  imports: [TypeOrmModule.forFeature([MovieEntity])],
   controllers: [MovieController],
   providers: [MovieService],
 })
