835aa4d4ac6418241c3ccbb7d3604e2491093a89
seojin <1020seojin@naver.com>
2024-08-23 15:41:05 +0900

    movie.repository CRUD 형식으로 수정

--- src/res/movie/repository/movie.repository.ts
+++ src/res/movie/repository/movie.repository.ts
@@ -1,39 +1,40 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Between, LessThan, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { Movie } from '../entity/movie.entity';
+import { MovieRequestDto } from '../dto/movie-request.dto';
 
 @Injectable()
 export class MovieRepository {
   constructor(
     @InjectRepository(Movie)
-    private readonly movieRepository: Repository<Movie>,
+    private readonly repository: Repository<Movie>,
   ) {}
 
-  async findAllOrderByModifiedAtDesc(): Promise<Movie[]> {
-    return this.movieRepository.find({
-      order: {
-        modifiedAt: 'DESC',
-      },
-    });
+  // 영화 생성 (Create)
+  async createMovie(movieRequestDto: MovieRequestDto): Promise<Movie> {
+    const movie = this.repository.create(movieRequestDto); 
+    return await this.repository.save(movie);
   }
 
-  async findMoviesReleasedBefore(date: Date): Promise<Movie[]> {
-    return this.movieRepository.find({
-      where: {
-        releasedAt: LessThan(date),
-      },
-    });
+  // 영화 전체 조회 (Read)
+  async findAllMovies(): Promise<Movie[]> {
+    return await this.repository.find();
   }
 
-  async findMoviesReleasedBetween(
-    startDate: Date,
-    endDate: Date,
-  ): Promise<Movie[]> {
-    return this.movieRepository.find({
-      where: {
-        releasedAt: Between(startDate, endDate),
-      },
-    });
+  // 특정 영화 조회 (Read)
+  async findMovieById(id: number): Promise<Movie> {
+    return await this.repository.findOne({ where: { id } }); 
+  }
+
+  // 영화 업데이트 (Update)
+  async updateMovie(id: number, movieRequestDto: MovieRequestDto): Promise<Movie> {
+    await this.repository.update(id, movieRequestDto); 
+    return await this.findMovieById(id);
+  }
+
+  // 영화 삭제 (Delete)
+  async deleteMovie(id: number): Promise<void> {
+    await this.repository.delete(id); 
   }
 }
