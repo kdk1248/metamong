a9c7861c60c29f4dcbfd411776117fd27830d2e9
kdk1248 <alice3976@g.hongik.ac.kr>
2024-08-23 18:24:54 +0900

    userentity/movieentity 가져오는거 주석 -> 삭제

--- src/res/favorite/favorite.module.ts
+++ src/res/favorite/favorite.module.ts
@@ -3,8 +3,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { FavoriteController } from './controller/favorite.controller';
 import { Favorite } from './entity/favorite.entity';
 import { FavoriteService } from './service/favorite.service';
-// import { UserEntity } from '../user/user.entity';  // UserEntity를 가져옵니다.
-// import { MovieEntity } from '../movie/movie.entity';  // MovieEntity를 가져옵니다.
 
 @Module({
   imports: [TypeOrmModule.forFeature([Favorite])],
