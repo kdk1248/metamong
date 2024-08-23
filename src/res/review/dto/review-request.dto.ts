1eaa943981ba0571f9f4686999f98b74552f6f24
kdk1248 <alice3976@g.hongik.ac.kr>
2024-08-23 17:41:16 +0900

    문서서식

--- src/res/review/dto/review-request.dto.ts
+++ src/res/review/dto/review-request.dto.ts
@@ -1,7 +1,6 @@
 export class ReviewRequestDto {
-    user: string;
-    content: string;
-    movieId: any;
+  user: string;
+  content: string;
+  movieId: any;
   username: import("c:/metaverse/movie/src/res/user/entity/user.entity").User;
-  }
-  
+}
