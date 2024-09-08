952e1be0a430505e4909fcfd7af80f6f68e5760b
kdk1248 <alice3976@g.hongik.ac.kr>
2024-09-08 16:54:53 +0900

    httpmodule 경로 추가

--- src/res/auth/auth.module.ts
+++ src/res/auth/auth.module.ts
@@ -2,13 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { HttpModule } from '@nestjs/axios'; // 추가된 부분
 import * as dotenv from 'dotenv';
 import { JwtStrategy } from './strategy/jwt.strategy';
 import { KakaoStrategy } from './strategy/kakao.strategy';
 import { User } from '../user/entity/user.entity';
 import { AuthController } from './controller/auth.controller';
 import { AuthService } from './service/auth.service';
-
 
 dotenv.config();
 
@@ -17,11 +17,12 @@ dotenv.config();
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret: process.env.JWT_SECRET,
-      signOptions:{
-        expiresIn: parseInt(process.env.JWT_EXPIRATION, 10)
-      }  
+      signOptions: {
+        expiresIn: parseInt(process.env.JWT_EXPIRATION, 10),
+      },
     }),
     TypeOrmModule.forFeature([User]),
+    HttpModule, // HttpModule 추가
   ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy, KakaoStrategy],
