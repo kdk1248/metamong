69f2ff859b79998b3f8a5d453d925e0eb35399f1
kdk1248 <alice3976@g.hongik.ac.kr>
2024-09-08 16:40:29 +0900

    프로퍼티 수정

--- src/res/user/entity/user.entity.ts
+++ src/res/user/entity/user.entity.ts
@@ -1,31 +1,57 @@
-import { BaseEntity } from "src/res/common/base.entity";
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
-
+import { Column, Entity, OneToMany } from 'typeorm';
+import { CommonBigPKEntity } from 'src/res/common/entity/common.entity';
+import { SignupRequestDto } from '../dto/user-request.dto';
+import { ReviewReply } from 'src/res/reviewreply/entity/reviewreply.entity';
+import { Favorite } from 'src/res/favorite/entity/favorite.entity';
+import { Review } from 'src/res/review/entity/review.entity';
+import { UserRole } from '../enum/user-role.enum';
 
 @Entity()
-export class User extends BaseEntity{
-    @Column()
-    username: string;
-
-    @Column()
-    password: string;
-
-    @Column({ unique: true }) // 이메일은 중복되지 않도록 한다.
+export class User extends CommonBigPKEntity {
+    @Column('varchar', { unique: true, nullable: false })
     email: string;
 
-    // @Column({ default: UserRole.USER })
-    // role: UserRole;
-
-    @Column({ nullable: true })
-    postalCode: string;
-
-    @Column({ nullable: true })
-    address: string;
-
-    @Column({ nullable: true })
-    detailAddress: string;
-    role: import("c:/metaverse/movie/src/res/user/enum/user-role.enum").UserRole;
+    @Column('varchar', { nullable: false })
     name: string;
+
+    @Column('varchar', { nullable: false })
     phonenumber: string;
 
+    @Column('varchar', { nullable: false })
+    password: string;
+
+    @Column()
+    role: UserRole;
+  
+    @Column()
+    postalCode: string;
+
+    @OneToMany(() => Review, (review) => review.user)
+    comments: Review[];
+
+    @OneToMany(() => Favorite, (favorite) => favorite.user)
+    favorite: Favorite[];
+
+    @OneToMany(() => ReviewReply, (reviewreply) => reviewreply.user)
+    replies: ReviewReply[];username: string;
+    address: string;
+    detailAddress: string;
+;
+
+    constructor(signupRequestDto?: SignupRequestDto) {
+        super();
+        if (signupRequestDto) {
+            this.name = signupRequestDto.name;
+            this.email = signupRequestDto.email;
+            this.phonenumber = signupRequestDto.phoneNumber;
+            this.password = signupRequestDto.password;
+        }
+    }
+
+    update(signupRequestDto: SignupRequestDto): void {
+        this.name = signupRequestDto.name;
+        this.email = signupRequestDto.email;
+        this.phonenumber = signupRequestDto.phoneNumber;
+        this.password = signupRequestDto.password;
+    }
 }
