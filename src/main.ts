af5ce5c63f4ed434efb241457f7f91a284824e8d
kdk <alice3976@g.hongik.ac.kr>
2024-09-29 11:50:19 +0900

    환경에 따른 주소 주고, 필요함에 따라 쿠키 포함 요청 허용

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // // cookie parser 미들웨어 추가
   // app.use(cookieParser());
-  
+  app.enableCors({
+    origin: ['http://localhost:8100', 'http://localhost:4200'],
+    credentials: true,  // 필요한 경우 쿠키를 포함한 요청 허용
+  });
+
   await app.listen(process.env.SERVER_PORT);
   Logger.log(`Application Running on Port : ${process.env.SERVER_PORT}`)
 }
