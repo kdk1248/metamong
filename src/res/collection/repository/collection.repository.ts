9787da25853d8935cf58f2d69057eb3cda8a71e7
seojin <1020seojin@naver.com>
2024-09-29 11:09:41 +0900

    컬렉션 코드 수정

--- src/res/collection/repository/collection.repository.ts
+++ src/res/collection/repository/collection.repository.ts
@@ -1,18 +1,29 @@
 import { Injectable } from '@nestjs/common';
-import { Repository } from 'typeorm';
+import { In, Repository } from 'typeorm';
 import { CollectionRequestDto } from '../dto/collection-request.dto';
 import { Collection } from '../entity/collection.entity';
+import { Movie } from 'src/res/movie/entity/movie.entity';
+import { User } from 'src/res/user/entity/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class CollectionRepository extends Repository<Collection> {
 
-  // 컬렉션 생성
+  @InjectRepository(Movie)
+  private movieRepository: Repository<Movie>;
+
+  @InjectRepository(User)
+  private userRepository: Repository<User>;
+
+  // CREATE
   async createCollection(collectionRequestDto: CollectionRequestDto): Promise<Collection> {
+    const { name, movieIds, userIds } = collectionRequestDto;
+    
     const collection = this.create(collectionRequestDto);
     return this.save(collection);
   }
 
-  // 모든 컬렉션 조회
+  // READ
   async getCollections(): Promise<Collection[]> {
     return this.find({
       order: {
@@ -21,7 +32,6 @@ export class CollectionRepository extends Repository<Collection> {
     });
   }
 
-  // 특정 컬렉션 조회
   async getCollectionById(id: number): Promise<Collection> {
     const collection = await this.findOneBy({ id });
     if (!collection) {
@@ -30,18 +40,28 @@ export class CollectionRepository extends Repository<Collection> {
     return collection;
   }
 
-  // 컬렉션 업데이트
+  // UPDATE
   async updateCollection(id: number, collectionRequestDto: CollectionRequestDto): Promise<Collection> {
+    const { name, movieIds, userIds } = collectionRequestDto;
+
     const collection = await this.findOneBy({ id });
     if (!collection) {
       throw new Error(`게시물이 존재하지 않습니다`);
     }
 
-    this.merge(collection, collectionRequestDto);
+    const movies = await this.movieRepository.find({
+      where: { id: In(movieIds) },
+    });
+
+    const users = await this.userRepository.find({
+      where: { id: In(userIds) },
+    });
+
+    this.merge(collection, { name, movies, users });
     return this.save(collection);
   }
 
-  // 컬렉션 삭제
+  // DELETE
   async deleteCollection(id: number): Promise<void> {
     const result = await this.delete(id);
     if (result.affected === 0) {
