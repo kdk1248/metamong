4428eeec0dcc81267251f83a18f02cdb87743c73
청야 <netjjang990126@gmail.com>
2024-10-02 16:48:43 +0900

    수작업으로 커밋

--- src/res/user/dto/user-response.dto.ts
+++ src/res/user/dto/user-response.dto.ts
@@ -1,11 +1,12 @@
-// // user-response.dto.ts
+// src/res/user/dto/user-response.dto.ts
 
-// export class LoginResponseDto {
-//     message: string;
-//     token?: string;
+// LoginResponseDto 클래스 정의
+export class LoginResponseDto {
+    message: string; // 응답 메시지
+    token?: string; // JWT 토큰 (선택적)
 
-//     constructor(message: string, token?: string) {
-//         this.message = message;
-//         this.token = token;
-//     }
-// }
+    constructor(message: string, token?: string) {
+        this.message = message; // 메시지 초기화
+        this.token = token; // 토큰 초기화 (기본값은 undefined)
+    }
+}
