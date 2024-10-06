10ebfb94c8e6621ac9c10fa79a14e091727b5656
kdk1248 <alice3976@g.hongik.ac.kr>
2024-10-06 10:21:36 +0900

    사용하는 모듈들 추가

--- src/res/comment/comment.module.ts
+++ src/res/comment/comment.module.ts
@@ -1,13 +1,18 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommentService } from './service/comment.service';
 import { Comment } from './entity/comment.entity';
 import { User } from 'src/res/user/entity/user.entity';
 import { CommentRepository } from './repository/comment.repository';
 import { CommentController } from './controller/comment.controller';
+import { UserModule } from '../user/user.module';
+import { MovieModule } from '../movie/movie.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Comment, User])], // Comment와 User 엔티티를 TypeORM 모듈에 등록
+  imports: [TypeOrmModule.forFeature([Comment]),
+  forwardRef(() =>UserModule),
+  MovieModule,
+], 
   controllers: [CommentController],
   providers: [CommentService, CommentRepository], // CommentService와 CommentRepository를 providers에 등록
   exports: [CommentService], // 필요시 다른 모듈에서도 CommentService를 사용할 수 있도록 export
