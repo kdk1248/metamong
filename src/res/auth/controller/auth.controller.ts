d379f7b0a6c22069debc779af6328116727ae806
Taein Jang <80192692+roblej@users.noreply.github.com>
2024-10-13 18:24:03 +0900

    회원탈퇴로직수정

--- src/res/auth/controller/auth.controller.ts
+++ src/res/auth/controller/auth.controller.ts
@@ -1,8 +1,6 @@
-import { Body, Controller, Get, Logger, Post, Query, Req, Res, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Logger, Param, Post, Query, Req, Res, UseGuards } from '@nestjs/common';
 import { Response } from 'express';
-
 import { AuthGuard } from '@nestjs/passport';
-
 import { AuthService } from '../service/auth.service';
 import { SignUpRequestDto } from '../dto/sign-up-request.dto';
 import { SignInRequestDto } from '../dto/sign-in-request.dto';
@@ -41,6 +39,7 @@ export class AuthController {
 
         return res.status(200).json(new ApiResponse(true, 200, 'Sign in successful', { user: userResponseDto, token: jwtToken }));
     }
+
     @Post('/logout')
     async logout(@Res() res: Response): Promise<any> {
         this.logger.verbose('User logging out');
@@ -51,7 +50,6 @@ export class AuthController {
         // Send a successful response
         return res.status(200).json(new ApiResponse(true, 200, 'Logout successful'));
     }
-
 
     // 인증된 회원이 들어갈 수 있는 테스트 URL 경로
     @Post('/test')
@@ -72,12 +70,27 @@ export class AuthController {
     // 카카오 로그인 콜백 엔드포인트
     @Get('kakao/callback')
     async kakaoCallback(@Query('code') kakaoAuthResCode: string, @Res() res: Response) {
-        const { jwtToken, user } = await this.authService.signInWithKakao(kakaoAuthResCode);
+        const { jwtToken, user } = await this.authService.signInWithKakao(kakaoAuthResCode); // AuthService에서 signInWithKakao 구현 필요
         const userResponseDto = new UserResponseDto(user);
 
         // Send JWT in the response header
         res.setHeader('Authorization', `Bearer ${jwtToken}`);
         this.logger.verbose(`User signed in successfully: ${JSON.stringify(userResponseDto)}`);
         res.status(200).json(new ApiResponse(true, 200, 'Sign in successful', { user: userResponseDto }));
+    }
+
+    // 사용자 삭제
+    @Delete('users/:id/delete') // @Post를 @Delete로 변경
+    async deleteUser(@Param('id') id: string, @Res() res: Response): Promise<any> {
+        this.logger.verbose(`Attempting to delete user with ID: ${id}`);
+
+        try {
+            await this.authService.deleteUser(id); // 성공 시 아무런 값을 반환하지 않음
+            this.logger.verbose(`User with ID ${id} deleted successfully`);
+            return res.status(200).json(new ApiResponse(true, 200, `User with ID ${id} deleted successfully`));
+        } catch (error) {
+            this.logger.error(`Failed to delete user with ID: ${id}`, error.stack);
+            return res.status(404).json(new ApiResponse(false, 404, `User with ID ${id} not found`));
+        }
     }
 }
